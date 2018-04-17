be4180b80b8638ab7c2e1c0beb236615d6cb48bc
Benny Hudson Hung <wakenightryu@gmail.com>
2018-04-17 06:03:19 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -8,7 +8,7 @@ module ArtistsHelper
     if song.artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "artist[artist_id]", song.artist_id
+      hidden_field_tag "song[artist_id]", song.artist_id
     end
   end
 
