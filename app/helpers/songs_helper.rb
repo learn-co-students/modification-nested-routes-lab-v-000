cab6ef361370fe10c139b9d5fd2494e39f966fd3
github username <github email address>
2018-04-20 16:12:07 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,14 @@
 module SongsHelper
   def artist_select(artist, song)
     if artist.nil?
-      select_tag("song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name))
+      select_tag("song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name), {include_blank: true})
     else
       hidden_field_tag("song[artist_id]", song.artist_id)
     end
+    # if artist
+    #   artist.name
+    # else
+    #   select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    # end
   end
 end
