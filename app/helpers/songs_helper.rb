b6526c18764d7c212445dc4c5af95d1a7ec6a13a
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-05-03 17:43:44 -0700

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+	def song_edit_display(song)
+		if song.artist.nil?
+			link_to "Edit Post", edit_song_path(song)
+		else 
+			link_to "Edit Post", edit_artist_song_path(@song.artist, @song) 
+		end 
+	end 
+			
 end
