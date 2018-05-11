bc6fdf5c297ed9526ec8cdd8cd0795a5954bda98
Connor Gallopo <connor.gallopo@me.com>
2018-05-11 10:22:35 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,8 @@
 module SongsHelper
-
+  def artist_select(artist, song)
+    if artist
+      artist.name
+    end
+    select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+  end
 end
