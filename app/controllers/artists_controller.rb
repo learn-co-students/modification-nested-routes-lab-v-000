7d28cdd3a7c2b80428926d9afd22ba7825b244b2
Araba <araba.taylor20@gmail.com>
2018-06-28 19:47:37 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -27,9 +27,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
