0aa46dcd6e5c6bf350c661e85ab16555377ffd7d
AlphaDaniel <daniel.nunez.nyc@gmail.com>
2018-07-11 20:34:53 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,70 +1,78 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:update, :destroy]
+  
   def index
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
-      else
-        @songs = @artist.songs
-      end
+    if artist_id
+      set_artist
+      @artist.nil? ? (redirect_to artists_path, alert: "Artist not found") : (@songs = @artist.songs)
     else
       @songs = Song.all
     end
   end
 
   def show
-    if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
-      @song = @artist.songs.find_by(id: params[:id])
-      if @song.nil?
-        redirect_to artist_songs_path(@artist), alert: "Song not found"
-      end
+    if artist_id
+      set_artist && set_artist_song
+      redirect_to artist_songs_path(@artist), alert: "Song not found" if @song.nil?
     else
-      @song = Song.find(params[:id])
+      set_song
     end
   end
 
   def new
-    @song = Song.new
+    artist_id && !Artist.exists?(artist_id) ? (redirect_to artists_path) : (@song = Song.new(artist_id: artist_id))
   end
 
   def create
     @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :new
-    end
+    @song.save ? (redirect_to @song) : (render :new)
   end
 
   def edit
-    @song = Song.find(params[:id])
-  end
-
-  def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
+    if artist_id
+      set_artist
+      
+      if @artist.nil?
+        redirect_to artists_path
+      else
+        set_artist_song
+        redirect_to artist_songs_path(@artist) if @song.nil?
+      end
+      
     else
-      render :edit
+      set_song
     end
   end
 
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
+  def update
+    @song.update(song_params)
+    @song.save ? (redirect_to @song) : (render :edit)
   end
 
-  private
+  def destroy
+    @song.destroy
+    redirect_to songs_path, alert: 'Song deleted.'
+  end
+
+private
+
+  def set_song
+    @song = Song.find_by(id: params[:id])
+  end
+  
+  def set_artist
+    @artist = Artist.find_by(id: artist_id)
+  end
+  
+  def set_artist_song
+    @song = @artist.songs.find_by(id: params[:id])
+  end
+  
+  def artist_id
+    params[:artist_id]
+  end
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,12 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+  
+  def artist_select(artist, song)
+    if artist
+      artist.name
+    else
+      collection_select(:song, :artist_id, Artist.all, :id, :name, include_blank: "Choose Artist", selected: nil)
+    end
+  end
 end
