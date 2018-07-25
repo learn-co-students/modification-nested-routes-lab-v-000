3d1204336217e21508b2987e85b9cca96e300df1
VictoriaMeng <1ststarofthenight@gmail.com>
2018-07-24 19:02:29 -0700

    completed songs controller tests

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,7 +45,15 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if Artist.exists?(id: params[:artist_id])
+      @artist = Artist.find(params[:artist_id])
+      if Song.exists?(id: params[:id])
+      else 
+        redirect_to artist_songs_path(@artist)
+      end
+    else 
+      redirect_to artists_path
+    end
   end
 
   def update
@@ -70,7 +78,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
