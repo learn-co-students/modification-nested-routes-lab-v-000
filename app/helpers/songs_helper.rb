1ace563de2681e39c93c443fddd404b225cbff8f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-31 22:53:54 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
-  def artist_field(song)
+  def artist_select(selection_group, song)
     if song.artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(selection_group, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist_id
     end
