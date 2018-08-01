6120a31a048e8cb5406914f4fc6e9a8ec314660f
taryn yaeger <tsyaeger@gmail.com>
2018-08-01 01:21:12 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -51,10 +51,35 @@ class SongsController < ApplicationController
 
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      
+      if author.nil? 
+        redirect_to artists_path, alert: "Artist not found."
+      else 
+        @post = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song not found" if song.nil?
+      end 
+      
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
 
+def edit
+  if params[:author_id]
+    author = Author.find_by(id: params[:author_id])
+    if author.nil?
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = author.posts.find_by(id: params[:id])
+      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+    end
+  else
+    @post = Post.find(params[:id])
+  end
+end
 
 
 
