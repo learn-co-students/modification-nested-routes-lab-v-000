fdcf059658d7e35d3c28877dad3da14e33a2bbae
taryn yaeger <tsyaeger@gmail.com>
2018-08-01 01:41:33 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,29 +2,25 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+  
+  def artist_select(artist, song)
+    if artist.nil?
+      if song.artist.nil?
+        select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+      else 
+        hidden_field_tag "song[artist_id]", song.artist_id 
+      end 
+    end
+  end
+  
 end
 
-
-#   before do
-#     Artist.destroy_all
-#     Song.destroy_all
-#     @artist = Artist.create!(name: "Daft Punk")
-#     @song = @artist.songs.create!(title: "The Grid")
-#   end
-
-#   subject { helper }
-
-#   it { should respond_to(:display_artist).with(1).argument }
-#   it { should respond_to(:artist_select).with(2).arguments }
-
-#   it "displays a link to edit the song if artist empty" do
-#     song = Song.create(title: "Bohemian Rhapsody")
-#     expect(helper.display_artist(song)).to include(edit_song_path(song))
-#     expect(helper.display_artist(song)).to include("Add Artist")
-#   end
-
-#   it "displays a link to the artist page if artist not empty" do
-#     expect(helper.display_artist(@song)).to include(artist_path(@artist))
-#     expect(helper.display_artist(@song)).to include(@artist.name)
+# module PostsHelper
+#   def author_id_field(post)
+#     if post.author.nil?
+#       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+#     else
+#       hidden_field_tag "post[author_id]", post.author_id
+#     end
 #   end
 # end
