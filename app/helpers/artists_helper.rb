34f652b9883110a2d9679faa7822d42bb5f1370b
taryn yaeger <tsyaeger@gmail.com>
2018-08-01 02:06:12 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
   end
   
   def artist_select(artist, song)
-    if artist
+    if artist.nil?
       if song.artist.nil?
         select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
       else 
