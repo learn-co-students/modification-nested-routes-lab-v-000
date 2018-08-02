8840810d29dc908f056e813e7f2e120cece2a0b1
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-02 14:19:43 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def new
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
-      redirect_to authors_path
+      redirect_to artists_path
     else
       @song = Song.new(artist_id: params[:artist_id])
     end
