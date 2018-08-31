930e2d310e4eb94adb4bce3e904151e7b22b05a1
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-31 22:28:34 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -6,15 +6,11 @@ module ArtistsHelper
   end
   
   def artist_select(song, artist)
-
-binding.pry
- 
     if artist
       hidden_field_tag "song[artist_id]", artist.id
     else
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
-
   end
   
 end
