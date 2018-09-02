ae47732ac684ec7cbb173473d9e40ceed4fc7f8f
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-01 21:41:20 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
 
+  def artist_select(song, artist)
+    if !!params[:artist_id]
+      artist.name
+    elsif !!song.artist
+      song.artist.name
+    else
+      collection_select(:song, :artist_id, Artist.all, :id, :name, {include_blank: ""})
+    end
+  end
+
 end
