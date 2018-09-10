640c1cf20f2b117dc1b1ec268b17ee638194cf2d
pcherian10 <pcherian10@gmail.com>
2018-09-09 22:22:16 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,11 @@
 module ArtistsHelper
+
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist, song)
+    link_to "Edit Song", edit_artist_song_path(@song.artist, @psong) if @song.artist
+  end
+
 end
