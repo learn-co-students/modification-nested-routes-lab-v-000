b7851f1019c9697eb283d7ef94e49a1f08651a1d
JCesana <joshuacesana@gmail.com>
2018-09-10 19:30:33 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,10 +4,10 @@ module ArtistsHelper
   end
 
   def artist_select(song, artist)
-    if artist.nil?
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    else
+    if artist
       hidden_field_tag "song[artist_id]", song.artist_id
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 end
