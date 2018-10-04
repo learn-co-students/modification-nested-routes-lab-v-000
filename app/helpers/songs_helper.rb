c46450233e756a0699d36461483de5262dcce157
mseaman6 <monicaameeti@gmail.com>
2018-10-04 13:32:37 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def artist_select(song, artist)
+    if artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      link_to artist.name, artist_path(artist)
+    end
+  end
 end
