c309cfc0a7895247666417dfbf1ad5ffbcf67c5f
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-10 01:00:24 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,8 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
+    if params[:artist_id] && Artist.find_by(id: params[:artist_id]).nil?
+      redirect_to artists_path
+    else
     @song = Song.new(artist_id: params[:artist_id])
   end
+end
 
   def create
     @song = Song.new(song_params)
