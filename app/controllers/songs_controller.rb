f59a450f8e94cb9a914a0e1730c614ed80e528b1
Anu Khambete <anukhambete@gmail.com>
2018-10-22 17:52:18 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
@@ -25,7 +26,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    #binding.pry
+    #@song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+    redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -64,7 +71,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
