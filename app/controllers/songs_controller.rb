71d6a0ed6ca66bd4836292826dbff639e32c672f
emanuelbierman <emanuel.bierman@gmail.com>
2018-10-30 11:36:40 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,16 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id]
+      if Artist.exists?(params[:artist_id])
+        @artist = Artist.find_by(id: params[:artist_id])
+        @song = Song.new(artist_id: params[:artist_id])
+      else
+        redirect_to artists_path, alert: "Artist not found"
+      end
+    else
+      @song = Song.new
+    end
   end
 
   def create
@@ -64,7 +73,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_id)
   end
 end
-
--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,8 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist)
+    song.artist.nil? ? collection_select(:song, :name, Artist.all, :id, :name) : display_artist(song)
+  end
 end
