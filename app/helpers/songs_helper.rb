5cc23bb0eae1fe5d32ff012e542088669e6b6884
micahshute <micah.shute@gmail.com>
2018-11-05 23:47:57 -0600

    helper unfinished

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+    def artist_select(song)
+        if song.artist.nil?
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+            "<p>#{}"
+        end
+    end
 end
