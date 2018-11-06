f20734d3833d8f225de6180def5361fa2dea498e
micahshute <micah.shute@gmail.com>
2018-11-06 08:32:48 -0600

    switching

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,4 @@
 module SongsHelper
 
-    def artist_select(song)
-        if song.artist.nil?
-            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-        else
-            "<p>#{}"
-        end
-    end
+   
 end
