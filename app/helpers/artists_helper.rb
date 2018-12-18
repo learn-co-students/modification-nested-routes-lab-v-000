985afac5bcb9aa425685c89a3122d21079af03c5
github username <github email address>
2018-12-17 20:14:40 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,14 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist, song)
+    # select_tag(name, option_tags = nil, options = {})
+    # select_tag "people", options_from_collection_for_select(@people, "id", "name")
+    # <select id="people" name="people"><option value="1">David</option></select>
+    # binding.pry
+    select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+
+    # <%= select_tag "date", options_for_select(["Today", "Old News"]), include_blank: true %>
+  end
 end
