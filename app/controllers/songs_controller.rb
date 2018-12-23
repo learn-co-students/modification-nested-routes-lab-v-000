94d955b1175942e75a9040cc5fa4950aabc4bb7a
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-12-23 12:52:45 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,9 +42,23 @@ class SongsController < ApplicationController
     end
   end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
+  # def edit
+  #   @song = Song.find(params[:id])
+  # end
+
+  def edit 
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil? 
+        redirect_to artists_path, alert: "Artist not found"
+      else 
+        @song = @artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(@artist), alert: "Song not found" if @song.nil?
+      end
+    else 
+      @song = Song.find_by(id: params[:id])
+    end
+  end 
 
   def update
     @song = Song.find(params[:id])
