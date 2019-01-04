8f5daf617e8e6a99601a031bfab5426fb15f326b
Vibraniumforge <gregm9999@gmail.com>
2019-01-03 20:30:28 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -2,7 +2,7 @@ module SongsHelper
 
     def artist_select(song, artist)
         if artist
-            hidden_field_tag "song[artist_id]", song.artist_id
+            artist.name
         else
             select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
         end
