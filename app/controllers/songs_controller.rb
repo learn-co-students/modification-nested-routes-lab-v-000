d4c536f3c899f0db21df4b5200daefd66912b60a
Ayana Bando <ayanaflax@yahoo.com>
2019-02-13 03:09:57 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,9 +27,9 @@ class SongsController < ApplicationController
   def new
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
     redirect_to artists_path, alert: "Artist not found."
-  else
-    @song = Song.new(artist_id: params[:artist_id])
-  end
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -78,7 +78,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
