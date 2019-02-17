63df653986af58b0ae3b0af9310045428a86bae1
Judy McGoldrick <jjeanius@verizon.net>
2019-02-17 21:29:30 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,12 +47,13 @@ class SongsController < ApplicationController
         redirect_to artists_path
       else
         @song = artist.songs.find_by(id: params[:id])
-        redirect_to artist_songs_path(artist)  if @song.nil?
+        redirect_to artist_songs_path(artist)
       end
     else
       @song = Song.find(params[:id])
     end
   end
+
 
   def update
     @song = Song.find(params[:id])
