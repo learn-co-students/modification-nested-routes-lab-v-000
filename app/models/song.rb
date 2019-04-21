225e714a268db7baa282a63880e22a02fa160839
Ashley <ashleywong678@gmail.com>
2019-04-21 15:01:04 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,4 +9,13 @@ class Song < ActiveRecord::Base
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
   end
+
+  def artist_id_field(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist._id
+    end
+  end
+
 end
