486d2937b79f7243d13442f64db2a837fdaadc23
Jonathan Luk <jluk87@gmail.com>
2019-05-01 23:35:03 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -5,6 +5,7 @@ module SongsHelper
       select_tag 'song[artist_id]', options_from_collection_for_select(artists, :id, :name)
     else
       hidden_field_tag 'song[artist_id]', song.artist_id
+
     end
   end
 
