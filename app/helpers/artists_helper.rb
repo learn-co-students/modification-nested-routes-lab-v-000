5db46ee4ac56e3a3c8d6d4d199bb225a1d8a26ce
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-05 14:44:09 -0400

    Refactor artists helper

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,7 +7,7 @@ module ArtistsHelper
     if artist
       artist.name
     else
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name, include_blank: "Select One")
     end
   end
 end
