0ccc54114aa5bdee18c969c99516221f8318ff3f
Sushi Chanrai <sushi@pathworks.co>
2019-08-19 16:11:16 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,9 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
 end
+
+
+#display a drop-down list of artists if someone edits a song directly via /songs/id/edit
+#only display the artist's name if they are editing through nested routing
