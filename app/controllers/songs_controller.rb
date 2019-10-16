ce97e3b9da7ead4f0431129777eb7a7f709fe875
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-10-16 10:55:14 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,31 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+
+    # deleted code
+    # @song = Song.new
+
+    # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-10-routes-and-resources/modifying-nested-resources-lab
+    # Using nested resources, set up routes and controller actions to create
+    # new song records through an artist.
+    # Hint: Don't forget to update the strong parameters.
+
+    # Set up routes and controller actions to support editing a song as a
+    # nested resource of an artist.
+
+    # new code start
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      # # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-10-routes-and-resources/modifying-nested-resources-lab
+      # Validate that new songs created for an artist via nested routing are
+      # created for valid artists, and redirect to /artists if not.
+      # 2.3.3 :002 > app.artists_path
+      # => "/artists" (artists_path is the form helper, "/artists" is the URL path)
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
+    # new code end
+
   end
 
   def create
@@ -39,7 +63,29 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+
+    # deleted code
+    # @song = Song.find(params[:id])
+
+    # new code start
+    if params[:artist_id]
+      # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-10-routes-and-resources/modifying-nested-resources-lab
+      # Validate that songs being edited via nested routing have a valid artist....
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        # Redirect to /artists if not.
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        # Validate that songs being edited via nested routing are in the artist's songs collection....
+        @song = artist.songs.find_by(id: params[:id])
+        # ...Redirect to /artists/id/songs if not.
+        redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+    end
+    # new code end
+
   end
 
   def update
@@ -64,7 +110,17 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+
+    # deleted code
+    # params.require(:song).permit(:title, :artist_name)
+
+    # https://learn.co/tracks/full-stack-web-development-v8/module-13-rails/section-10-routes-and-resources/modifying-nested-resources-lab
+    # Using nested resources, set up routes and controller actions to create
+    # new song records through an artist.
+    # Hint: Don't forget to update the strong parameters.  (Updates below:)
+    # new code start
+    params.require(:song).permit(:title, :artist_name, :artist_id)
+    # new code end
+
   end
 end
-
