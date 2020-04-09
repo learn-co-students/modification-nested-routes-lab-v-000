3d5035cd7a3fec880b69f0beeed63c1206bcb399
Tyler Sell <tylerselldesign@gmail.com>
2020-04-09 16:20:30 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
 
+  def artist_select(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      label_tag song.artist.name
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
+
 end
