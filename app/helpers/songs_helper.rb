14364300d8d086b81056ac2969837468f49e5b70
Laura Messa Travin <laura.travin@gmail.com>
2021-03-28 02:03:15 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -9,11 +9,5 @@ module SongsHelper
      end
 
 
-    #  def artist_select(artist, song)
-    #    if artist
-    #      artist.name
-    #    else
-    #      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    #    end
-    #  end
+
 end
